7b767d79fde016a13734bb1cf3a6ea17de70adf7
senda-tomoko <156554698+senda-tomoko@users.noreply.github.com>
2024-01-31 20:28:00 +0900

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    let display = $('#display'); 
+
+   
+    $('.number, .decimal').click(function() {
+        let currentDisplay = display.text();
+        let newValue = $(this).text();
+
+        if (currentDisplay === '0' && newValue !== '.') {
+            display.text(newValue);
+        } else {
+            display.text(currentDisplay + newValue);
+        }
+    });
+
+    $('.add, .subtract, .multiply, .divide').click(function() {
+        let currentDisplay = display.text();
+        let operator = $(this).text();
+
+        if (!currentDisplay.endsWith('+') && !currentDisplay.endsWith('-') && !currentDisplay.endsWith('*') && !currentDisplay.endsWith('/')) {
+            display.text(currentDisplay + operator);
+        }
+    });
+
+    $('.equals').click(function() {
+        let calculation = display.text();
+        try {
+            let result = eval(calculation);
+            display.text(result);
+        } catch (e) {
+            display.text('Error');
+        }
+    });
+
+
+    $('.all.clear').click(function() {
+        display.text('0');
+    });
+
+
+    $('.zero').not('.double').click(function() {
+        let currentDisplay = display.text();
+        if (currentDisplay !== '0') { 
+            display.text(currentDisplay + '0');
+        }
+    });
+
+
+    $('.double.zero').click(function() {
+        let currentDisplay = display.text();
+        if (currentDisplay !== '0') { 
+            display.text(currentDisplay + '00');
+        }
+    });
+
+    
+    $('.decimal').click(function() {
+        let currentDisplay = display.text();
+        if (!currentDisplay.includes('.')) { 
+            display.text(currentDisplay + '.');
+        }
+    });
+});
